721ea9b8de50cfaf079fca359358c6716f9a83b3
myLeskrisik <smyles0763@icloud.com>
2022-08-24 10:53:58 -0700

    made all of the site more mobile friendly pt2

--- src/page_data.js
+++ src/page_data.js
@@ -4,13 +4,13 @@ export default {
       title: "Software Engineer Intern",
       company: "Commerce Architects",
       responsibilites: ["1", "2", "3"],
-      date: "June 2022 - August 2022",
+      date: "June 2022 - Aug 2022",
     },
     {
       title: "Project Lead, Member",
       company: "Dubvelopers",
       responsibilites: ["1", "2", "3"],
-      date: "Jan. 2022 - June 2022",
+      date: "Jan 2022 - June 2022",
     },
   ],
   projects: [
